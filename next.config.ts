8c405bfe29ba9f9469d43766f4905f8a1fdf4f15
Pankaj Kumar Roy <1719pankaj@gmail.com>
2025-08-25 21:54:55 +0530

    added C# cfg fixed vercel dep6

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,17 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '*.public.blob.vercel-storage.com',
+        port: '',
+        pathname: '/**',
+      },
+      // Keep your existing patterns if any
+    ],
+  },
   async rewrites() {
     return [
       {
